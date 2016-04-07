e2e4db633c3925f9478beac692d0c7ea8037f6e2
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-04-07 14:43:44 +0200

    first working implementation of a stream filter function

--- FilterStreamByMarkers.m
+++ FilterStreamByMarkers.m
@@ -1,7 +1,25 @@
-function [ output_args ] = FilterStreamByMarkers( stream, markerset )
-%FILTERSTREAMBYMARKER Summary of this function goes here
-%   Detailed explanation goes here
+function filteredStream = FilterStreamByMarkers( stream, markerPattern )
+%FILTERSTREAMBYMARKER get a subset of elements from the stream by 
+%   using patterns describing the markers you want
 
+lengthOfAllSamples = length(stream.time_series);
+samples = stream.time_series;
+timeStamps = stream.time_stamps;
+indexOfFilteredStream = 1
 
+for i=1 : lengthOfAllSamples
+    currentMarker = samples{i};
+    for m=1 : numel(markerPattern)
+      currentPattern = markerPattern{m};
+      match = regexp(currentMarker, currentPattern, 'match');
+      if ~isempty(match)
+        selectedElements(indexOfFilteredStream).timeStamp = timeStamps(i);
+        selectedElements(indexOfFilteredStream).marker = samples(i);
+        indexOfFilteredStream = indexOfFilteredStream + 1;
+      end
+    end
+end
+
+    filteredStream = selectedElements
 end
 
